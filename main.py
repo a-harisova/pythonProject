86d7436f36080e0132019e65b58437c896d4c21c
a-harisova <kharisova-adilya@yandex.ru>
2023-05-02 01:29:07 +0300

    add route

--- main.py
+++ main.py
@@ -2,14 +2,38 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/index")
-@app.route("/")
-def index():
-    return "index"
+@app.route('/')
+def render_main():
+    return "здесь будет главная"
 
-@app.route("/about")
-def about():
-    return "О сайте"
+@app.route('/teachers/')
+def render_teachers():
+    return "здесь будут преподаватели"
+
+@app.route('/goals/<string:goal>/')
+def render_goals(goal):
+    return "здесь будет цель <goal>"
+
+@app.route('/profiles/<int:teacher_id>/')
+def render_profiles(teacher_id):
+    return "здесь будет преподаватель <id учителя>"
+
+@app.route('/request/')
+def render_request():
+    return "здесь будет заявка на подбор"
+
+@app.route('/request_done/')
+def render_request_done():
+    return "заявка на подбор отправлена"
+
+@app.route('/booking/<int:teacher_id>/<day>/<time>/')
+def render_booking(teacher_id, day, time):
+    return "здесь будет форма бронирования <id учителя>"
+
+@app.route('/booking_done/')
+def render_booking_done():
+    return "заявка отправлена"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
